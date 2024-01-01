e0a9095f6e77b23ecf72729c678479a91fe8eb03
iltenahmet <iltenahmet@gmail.com>
2024-01-01 19:28:30 +0300

    Guessing game fini

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -5,4 +5,10 @@ fn main() {
     println!("Please input your guess:");
     let mut guess = String::new();
 
+    io::stdin() 
+        .read_line(&mut guess)
+        .expect("Failed to read the line");
+
+    println!("You guessed: {guess}");
+
 }
